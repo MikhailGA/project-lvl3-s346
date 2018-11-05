fde0240fa5c14391683ec165f3cd715721a959c7
Mihail <freon2006@bk.ru>
2018-11-06 03:44:08 +0500

    new project

--- src/bin/open.js
+++ src/bin/open.js
@@ -1,12 +1,18 @@
-import program from 'commander';
+#!/usr/bin/env node
+
+// import program from 'commander';
 import pageLoader from '..';
 
-program
-  .description('Open file')
-  .arguments('<path>')
-  .action((path) => {
-    pageLoader.open(path)
-      .then(data => console.log(data))
-      .catch(err => console.log(err));
-  })
-  .parse(process.argv);
+// program
+//   .description('Open file')
+//   .arguments('<path>')
+//   .action((path) => {
+//     pageLoader.open(path)
+//       .then(data => console.log(data))
+//       .catch(err => console.log(err));
+//   })
+//   .parse(process.argv);
+const path = process.argv[2];
+pageLoader.open(path)
+  .then(data => console.log(data))
+  .catch(err => console.log(err));
